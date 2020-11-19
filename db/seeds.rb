c521352f6a00d313a05e84c3e0ce8bd62c552569
morganfula <morgan.fula@me.com>
2020-11-19 10:40:03 +0100

    doses controller created

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,6 +18,6 @@ puts "Create ingredients"
 url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
 ingredients = JSON.parse(open(url).read)
 ingredients["drinks"].each do |ingredient|
-  i = Ingredient.create(name: ingredient["strIngredient1"])
+  i = Ingredient.create!(name: ingredient["strIngredient1"])
   puts "create #{i.name}"
 end
